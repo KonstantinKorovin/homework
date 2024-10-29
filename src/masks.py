00529d08432bf01dcb36ba4f855b5643d6217a52
Konstantin Korovin <ryan337@mail.ru>
2024-10-29 18:18:23 +0300

    Результаты прошлой домашки

---
+++ src/masks.py
@@ -0,0 +1,21 @@
+from typing import Any
+
+
+def get_mask_card_number(card_number: Any = str) -> str:
+    """Функция принимает на вход номер карты и возвращает ее маску"""
+
+    if len(card_number) != 16:
+        return "Неверный формат номера карты"
+    masked_card_number = (
+        f"{card_number[:4]} {card_number[4:6]}** **** {card_number[12:]}"
+    )
+    return masked_card_number
+
+
+def get_mask_account(account_number: Any = str) -> str:
+    """Функция принимает на вход номер счета и возвращает его маску"""
+
+    if len(account_number) != 20:
+        return "Неверный формат номера счета"
+    masked_account_number = f"**{account_number[:-5]}"
+    return masked_account_number
