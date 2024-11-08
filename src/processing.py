2e95adca79e935939a1b24960134a53adb55bce6
Konstantin Korovin <ryan337@mail.ru>
2024-11-08 16:26:34 +0300

    homework 10_1 done

--- src/processing.py
+++ src/processing.py
@@ -1,21 +1,21 @@
-def filter_by_state(arg: list[dict[str]], state='EXECUTED') -> list[dict[str]]:
-    """ Функция возвращает новый список словарей, содержащий только те словари,
-    у которых ключ state соответствует указанному значению """
+def filter_by_state(arg: list[dict], state: str = "EXECUTED") -> list[dict]:
+    """Функция возвращает новый список словарей, содержащий только те словари,
+    у которых ключ state соответствует указанному значению"""
 
     dict_keys = []
 
     for key in arg:
-        if key.get('state') == state:
+        if key.get("state") == state:
             dict_keys.append(key)
     return dict_keys
 
 
-def sort_by_date(dicts: list[dict[str]], rev ='True') -> list[dict[str]]:
-    """ Функция принимает на вход список словарей и параметр порядка сортировки,
-    возвращает новый список, в котором исходные словари отсортированы по дате """
+def sort_by_date(dicts: list[dict], rev: str = "True") -> list[dict]:
+    """Функция принимает на вход список словарей и параметр порядка сортировки,
+    возвращает новый список, в котором исходные словари отсортированы по дате"""
 
     sorted_data = []
 
     if dicts:
-        sorted_data = sorted(dicts, key=lambda x: x.get('date'), reverse=rev)
+        sorted_data = sorted(dicts, key=lambda x: x.get("date"), reverse=rev)
     return sorted_data
