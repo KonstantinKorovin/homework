84cd33a09eedbb7d46fe5549058f455ead6515ad
Konstantin Korovin <ryan337@mail.ru>
2024-11-08 18:04:10 +0300

    Refactore processing function

--- src/processing.py
+++ src/processing.py
@@ -1,21 +1,21 @@
-def filter_by_state(arg: list[dict], state: str = "EXECUTED") -> list[dict]:
+def filter_by_state(list_dict_filter: list[dict], state: str = "EXECUTED") -> list[dict]:
     """Функция возвращает новый список словарей, содержащий только те словари,
     у которых ключ state соответствует указанному значению"""
 
-    dict_keys = []
+    clean_dict = []
 
-    for key in arg:
+    for key in list_dict_filter:
         if key.get("state") == state:
-            dict_keys.append(key)
-    return dict_keys
+            clean_dict.append(key)
+    return clean_dict
 
 
-def sort_by_date(dicts: list[dict], rev: str = "True") -> list[dict]:
+def sort_by_date(data_sort_dict: list[dict], date_sort: bool = True) -> list[dict]:
     """Функция принимает на вход список словарей и параметр порядка сортировки,
     возвращает новый список, в котором исходные словари отсортированы по дате"""
 
     sorted_data = []
 
-    if dicts:
-        sorted_data = sorted(dicts, key=lambda x: x.get("date"), reverse=rev)
+    if data_sort_dict:
+        sorted_data = sorted(data_sort_dict, key=lambda x: x.get("date"), reverse=date_sort)
     return sorted_data
