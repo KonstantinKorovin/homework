82f39dd51c794a4366e58233fb5258d80dae7495
Konstantin Korovin <ryan337@mail.ru>
2024-12-17 07:51:12 +0300

    very ref

--- src/external_api.py
+++ src/external_api.py
@@ -6,41 +6,30 @@ import requests
 from dotenv import load_dotenv
 
 load_dotenv()
+api_key = os.getenv("API_KEY")
 
 
 def get_transaction_amount(transaction: dict) -> Any:
     """Функция принимает на вход транзакцию и возвращает сумму транзакции (amount) в рублях, тип данных — float"""
-    if transaction["operationAmount"]["currency"]["code"] in "RUB":
-        return transaction["operationAmount"]["amount"]
-    elif transaction["operationAmount"]["currency"]["code"] in ["USD", "EUR"]:
-        api_key = os.getenv("API-KEY")
+    date_now = datetime.datetime.now()
+    date_string = date_now.strftime("%Y-%m-%d")
+    if (
+        transaction["operationAmount"]["currency"]["code"] in "USD"
+        or transaction["operationAmount"]["currency"]["code"] in "EUR"
+    ):
         url = (
-            f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&"
-            f'from={transaction["operationAmount"]["currency"]["code"]}&'
-            f'amount={transaction["operationAmount"]["amount"]}&date={datetime.datetime.now()}'
+            f"https://api.apilayer.com/exchangerates_data/convert?to={'RUB'}"
+            f"&from={transaction['operationAmount']['currency']['code']}"
+            f"&amount={transaction['operationAmount']['amount']}&date={date_string}"
         )
 
-        headers = api_key
+        headers = {"apikey": api_key}
+
         response = requests.request("GET", url, headers=headers)
         if response.status_code == 200:
-            data = response.json()
-            exchange_rate = data["info"]["rate"]
-            return float(transaction["operationAmount"]["amount"]) * float(exchange_rate)
-        return []
+            result = response.text
+            return result
+    elif transaction["operationAmount"]["currency"]["code"] in "RUB":
+        return f"{float(transaction['operationAmount']['amount'])} RUB"
     else:
         return []
-
-
-print(
-    get_transaction_amount(
-        {
-            "id": 41428829,
-            "state": "EXECUTED",
-            "date": "2019-07-03T18:35:29.512364",
-            "operationAmount": {"amount": "8221.37", "currency": {"name": "USD", "code": "USD"}},
-            "description": "Перевод организации",
-            "from": "MasterCard 7158300734726758",
-            "to": "Счет 35383033474447895560",
-        }
-    )
-)
--- src/transactions_read.py
+++ src/transactions_read.py
@@ -1,11 +1,18 @@
 import pandas as pd
-import openpyxl
+
+csv_df = pd.read_csv("/Users/krynik/PycharmProjects/bank_vidget/data/transactions.csv")
 
 
-cdf = pd.read_csv('/Users/krynik/PycharmProjects/bank_vidget/data/transactions.csv')
+def reading_csv_transactions(csv_df: pd.DataFrame) -> list[dict]:
+    """Функция для считывания финансовых операций из CSV выдает список словарей с транзакциями"""
+    csv_df_dict = csv_df.to_dict("records")
+    return csv_df_dict
 
 
-def reading_csv_transactions(cdf):
-    ''' Функция для считывания финансовых операций из CSV выдает список словарей с транзакциями '''
-    return cdf
-print(reading_csv_transactions(cdf))
+excel_df = pd.read_excel("/Users/krynik/PycharmProjects/bank_vidget/data/transactions_excel.xlsx")
+
+
+def reading_excel_transactions(excel_df: pd.DataFrame) -> list[dict]:
+    """Функция для считывания финансовых операций из Excel выдает список словарей с транзакциями"""
+    excel_df_dict = excel_df.to_dict("records")
+    return excel_df_dict
